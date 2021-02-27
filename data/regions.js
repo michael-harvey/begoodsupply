3be86498521fa188c191cfff14d62e50556fce1c
Mike Harvey <harvey.miche@gmail.com>
2021-02-27 19:42:43 +1300

    Adds regions data

---
+++ data/regions.js
@@ -0,0 +1,87 @@
+export default [
+  {
+    id: 1,
+    name: 'Wellington',
+    value: 'wellington',
+  },
+  {
+    id: 2,
+    name: 'Manawatu-wanganui',
+    value: 'manawatu-wanganui',
+  },
+  {
+    id: 3,
+    name: 'Taranaki',
+    value: 'taranaki',
+  },
+  {
+    id: 4,
+    name: "Hawke's bay",
+    value: 'hawkes-bay',
+  },
+  {
+    id: 5,
+    name: 'Gisborne',
+    value: 'gisborne',
+  },
+  {
+    id: 6,
+    name: 'Bay of plenty',
+    value: 'bay-of-plenty',
+  },
+  {
+    id: 7,
+    name: 'Waikato',
+    value: 'waikato',
+  },
+  {
+    id: 8,
+    name: 'Auckland',
+    value: 'auckland',
+  },
+  {
+    id: 9,
+    name: 'Northland',
+    value: 'northland',
+  },
+  {
+    id: 10,
+    name: 'Nelson',
+    value: 'nelson',
+  },
+  {
+    id: 11,
+    name: 'Tasman',
+    value: 'tasman',
+  },
+  {
+    id: 12,
+    name: 'Marlborough',
+    value: 'marlborough',
+  },
+  {
+    id: 13,
+    name: 'West Coast',
+    value: 'west-coast',
+  },
+  {
+    id: 14,
+    name: 'Canterbury',
+    value: 'canterbury',
+  },
+  {
+    id: 15,
+    name: 'Otago',
+    value: 'otago',
+  },
+  {
+    id: 16,
+    name: 'Southland',
+    value: 'southland',
+  },
+  {
+    id: 17,
+    name: 'Chatham Islands',
+    value: 'chatham-islands',
+  },
+];
