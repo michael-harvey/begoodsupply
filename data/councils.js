080af60d3bae5068c71ad78747565dd6a6235e39
Mike Harvey <harvey.miche@gmail.com>
2021-02-28 01:20:10 +1300

    Adds council data

---
+++ data/councils.js
@@ -0,0 +1,484 @@
+export default [
+  {
+    id: 1,
+    regionId: 1,
+    name: 'Carterton District Council',
+    value: 'carterton-district-council',
+  },
+  {
+    id: 2,
+    regionId: 1,
+    name: 'Kapiti Coast District Council',
+    value: 'kapiti-coast-district-council',
+  },
+  {
+    id: 3,
+    regionId: 1,
+    name: 'Masterton District Council',
+    value: 'masterton-district-council',
+  },
+  {
+    id: 4,
+    regionId: 1,
+    name: 'South Wairarapa District Council',
+    value: 'south-wairarapa-district-council',
+  },
+  {
+    id: 5,
+    regionId: 1,
+    name: 'Tararua District Council',
+    value: 'tararua-district-council',
+  },
+  {
+    id: 6,
+    regionId: 1,
+    name: 'Upper Hutt City Council',
+    value: 'Upper Hutt City Council',
+  },
+  {
+    id: 7,
+    regionId: 1,
+    name: 'Wellington City council',
+    value: 'Wellington City council',
+  },
+  {
+    id: 8,
+    regionId: 1,
+    name: 'Hutt City Council',
+    value: 'hutt-city-council',
+  },
+  {
+    id: 9,
+    regionId: 1,
+    name: 'Porirua City Council',
+    value: 'porirua-city-council',
+  },
+
+  // region id 2
+  {
+    id: 10,
+    regionId: 2,
+    name: 'Horowhenua District Council',
+    value: 'horowhenua-district-council',
+  },
+  {
+    id: 11,
+    regionId: 2,
+    name: 'Manawatu District Council',
+    value: 'manawatu-district-council',
+  },
+  {
+    id: 12,
+    regionId: 2,
+    name: 'Palmerston North City Council',
+    value: 'palmerston-north-city-council',
+  },
+  {
+    id: 13,
+    regionId: 2,
+    name: 'Rangitikei District Council',
+    value: 'rangitikei-district-council',
+  },
+  {
+    id: 14,
+    regionId: 2,
+    name: 'Ruapehu District Council',
+    value: 'ruapehu-district-council',
+  },
+  {
+    id: 15,
+    regionId: 2,
+    name: 'Tararua District Council',
+    value: 'tararua-district-council',
+  },
+  {
+    id: 16,
+    regionId: 2,
+    name: 'Taupo District Council',
+    value: 'taupo-district-council',
+  },
+  {
+    id: 17,
+    regionId: 2,
+    name: 'Waitomo District Council',
+    value: 'waitomo-district-council',
+  },
+  {
+    id: 18,
+    regionId: 2,
+    name: 'Whanganui District Council',
+    value: 'whanganui-district-council',
+  },
+
+  // region id 3
+  {
+    id: 19,
+    regionId: 3,
+    name: 'New Plymouth District Council',
+    value: 'new-plymouth-district-council',
+  },
+  {
+    id: 20,
+    regionId: 3,
+    name: 'South Taranaki District Council',
+    value: 'south-taranaki-district-council',
+  },
+  {
+    id: 21,
+    regionId: 3,
+    name: 'Stratford District Council',
+    value: 'stratford-district-council',
+  },
+
+  // region id 4
+  {
+    id: 22,
+    regionId: 4,
+    name: "Central Hawke's Bay District Council",
+    value: 'central-hawkes-bay-district Council',
+  },
+  {
+    id: 23,
+    regionId: 4,
+    name: 'Hastings District Council',
+    value: 'hastings-district-council',
+  },
+  {
+    id: 24,
+    regionId: 4,
+    name: 'Napier City Council',
+    value: 'napier-city-council',
+  },
+  {
+    id: 25,
+    regionId: 4,
+    name: 'Rangitikei District Council',
+    value: 'rangitikei-district-council',
+  },
+  {
+    id: 26,
+    regionId: 4,
+    name: 'Taupo District Council',
+    value: 'taupo-district-council',
+  },
+  {
+    id: 27,
+    regionId: 4,
+    name: 'Wairoa District Council',
+    value: 'wairoa-district-council',
+  },
+
+  // region id 5
+  {
+    id: 28,
+    regionId: 5,
+    name: 'Gisborne District Council',
+    value: 'gisborne-district-council',
+  },
+
+  // region id 6
+  {
+    id: 29,
+    regionId: 6,
+    name: 'Kawerau District Council',
+    value: 'kawerau-district-council',
+  },
+  {
+    id: 30,
+    regionId: 6,
+    name: 'Opotiki District Council',
+    value: 'opotiki-district-council',
+  },
+  {
+    id: 31,
+    regionId: 6,
+    name: 'Rotorua District Council',
+    value: 'rotorua-district-council',
+  },
+  {
+    id: 32,
+    regionId: 6,
+    name: 'Tauranga City Council',
+    value: 'tauranga-city-council',
+  },
+  {
+    id: 33,
+    regionId: 6,
+    name: 'Western Bay of Plenty District Council',
+    value: 'western-bay-of-plenty-district-council',
+  },
+  {
+    id: 34,
+    regionId: 6,
+    name: 'Whakatane District Council',
+    value: 'whakatane-district-council',
+  },
+
+  // region id 7
+  {
+    id: 35,
+    regionId: 7,
+    name: 'Hamilton City Council',
+    value: 'hamilton-city-council',
+  },
+  {
+    id: 36,
+    regionId: 7,
+    name: 'Hauraki District Council',
+    value: 'hauraki-district-council',
+  },
+  {
+    id: 37,
+    regionId: 7,
+    name: 'Matamata-piako District Council',
+    value: 'matamata-piako-district-council',
+  },
+  {
+    id: 38,
+    regionId: 7,
+    name: 'Otorohanga District Council',
+    value: 'otorohanga-district-council',
+  },
+  {
+    id: 39,
+    regionId: 7,
+    name: 'South Waikato District Council',
+    value: 'south-waikato-district-council',
+  },
+  {
+    id: 40,
+    regionId: 7,
+    name: 'Thames-coromandel District Council',
+    value: 'thames-coromandel-district-council',
+  },
+  {
+    id: 41,
+    regionId: 7,
+    name: 'Waikato District Council',
+    value: 'waikato-district-council',
+  },
+  {
+    id: 42,
+    regionId: 7,
+    name: 'Waipa District Council',
+    value: 'waipa-district-council',
+  },
+  {
+    id: 43,
+    regionId: 7,
+    name: 'Waitomo District Council',
+    value: 'waitomo-district-council',
+  },
+
+  // region id 8
+  {
+    id: 44,
+    regionId: 8,
+    name: 'Auckland Council',
+    value: 'Auckland Council',
+  },
+
+  // region id 9
+  {
+    id: 45,
+    regionId: 9,
+    name: 'Far North District Council',
+    value: 'far-north-district-council',
+  },
+  {
+    id: 46,
+    regionId: 9,
+    name: 'Kaipara District Council',
+    value: 'kaipara-district-council',
+  },
+  {
+    id: 47,
+    regionId: 9,
+    name: 'Whangarei District Council',
+    value: 'whangarei-district-council',
+  },
+
+  // region id 10
+  {
+    id: 48,
+    regionId: 10,
+    name: 'Nelson City Council',
+    value: 'nelson-city-council',
+  },
+
+  // region id 11
+  {
+    id: 49,
+    regionId: 11,
+    name: 'Tasman District Council',
+    value: 'Tasman District Council',
+  },
+
+  // region id 12
+  {
+    id: 50,
+    regionId: 12,
+    name: 'Marlborough District Council',
+    value: 'marlborough-district-council',
+  },
+
+  // region id 13
+  {
+    id: 51,
+    regionId: 13,
+    name: 'Buller City Council - Zone 1',
+    value: 'buller-city-council-zone-1',
+  },
+  {
+    id: 52,
+    regionId: 13,
+    name: 'Buller City Council - Zone 2',
+    value: 'buller-city-council-zone-2',
+  },
+  {
+    id: 53,
+    regionId: 13,
+    name: 'Buller City Council - Zone 3',
+    value: 'buller-city-council-zone-3',
+  },
+  {
+    id: 54,
+    regionId: 13,
+    name: 'Grey District Council',
+    value: 'grey-district-council',
+  },
+  {
+    id: 55,
+    regionId: 13,
+    name: 'Westland District Council',
+    value: 'westland-district-council',
+  },
+  {
+    id: 56,
+    regionId: 13,
+    name: 'Westland District Council - South',
+    value: 'westland-district-council-south',
+  },
+
+  // region id 14
+  {
+    id: 57,
+    regionId: 14,
+    name: 'Ashburton District Council',
+    value: 'ashburton-district-council',
+  },
+  {
+    id: 58,
+    regionId: 14,
+    name: 'Christchurch City Council',
+    value: 'christchurch-city-council',
+  },
+  {
+    id: 59,
+    regionId: 14,
+    name: 'Hurunui District Council',
+    value: 'hurunui-district-council',
+  },
+  {
+    id: 59,
+    regionId: 14,
+    name: 'Hurunui District Council - Hamner Springs',
+    value: 'hurunui-district-council-hamner-springs',
+  },
+  {
+    id: 60,
+    regionId: 14,
+    name: 'Kaikoura District Council',
+    value: 'kaikoura-district-council',
+  },
+  {
+    id: 61,
+    regionId: 14,
+    name: 'Mackenzie District Council',
+    value: 'mackenzie-district-council',
+  },
+  {
+    id: 62,
+    regionId: 14,
+    name: 'Selwyn District Council',
+    value: 'selwyn-district-council',
+  },
+  {
+    id: 63,
+    regionId: 14,
+    name: 'Timaru District Council',
+    value: 'timaru-district-council',
+  },
+  {
+    id: 64,
+    regionId: 14,
+    name: 'Waimakariri District Council',
+    value: 'waimakariri-district-council',
+  },
+  {
+    id: 65,
+    regionId: 14,
+    name: 'Waimate District Council',
+    value: 'waimate-district-council',
+  },
+  {
+    id: 66,
+    regionId: 14,
+    name: 'Waitaki District Council',
+    value: 'waitaki-district-council',
+  },
+
+  // region id 15
+  {
+    id: 67,
+    regionId: 15,
+    name: 'Central Otago District Council',
+    value: 'central-otago-district-council',
+  },
+  {
+    id: 68,
+    regionId: 15,
+    name: 'Clutha District Council',
+    value: 'clutha-district-council',
+  },
+  {
+    id: 69,
+    regionId: 15,
+    name: 'Dunedin City Council',
+    value: 'dunedin-city-council',
+  },
+  {
+    id: 70,
+    regionId: 15,
+    name: 'Queenstown-Lakes District Council',
+    value: 'queenstown-lakes-district-council',
+  },
+
+  // region id 16
+  {
+    id: 71,
+    regionId: 16,
+    name: 'Gore District Council',
+    value: 'gore-district-council',
+  },
+  {
+    id: 72,
+    regionId: 16,
+    name: 'Invercargill City Council',
+    value: 'invercargill-city-council',
+  },
+  {
+    id: 73,
+    regionId: 16,
+    name: 'Southland District Council',
+    value: 'southland-district-council',
+  },
+
+  // region id 17
+  {
+    id: 73,
+    regionId: 17,
+    name: 'Chatham Islands Council',
+    value: 'chatham-islands-council',
+  },
+];
