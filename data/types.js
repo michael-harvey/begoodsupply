9d61d6c1f751595eed54fd442c542f72a67db41c
Mike Harvey <harvey.miche@gmail.com>
2021-02-28 01:20:17 +1300

    Adds types data

---
+++ data/types.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: 1,
+    councilId: 1,
+    name: 'plastic1',
+    value: '004-WLG-KS',
+  },
+  {
+    id: 2,
+    councilId: 1,
+    name: 'plastic2',
+    value: '005-PMN-KS',
+  },
+  {
+    id: 3,
+    councilId: 1,
+    name: 'plastic3',
+    value: 'Landfill',
+  },
+];
