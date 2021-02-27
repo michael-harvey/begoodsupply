cdd57406b9cebd409970a3b7071d352d995a548c
Mike Harvey <harvey.miche@gmail.com>
2021-02-28 01:20:32 +1300

    Adds select dropdowns for region / council

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,83 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import { useState, useEffect } from 'react';
+
+import regions from '../data/regions';
+import councils from '../data/councils';
+import types from '../data/types';
 
 export default function Home() {
+  const [regionId, setRegionId] = useState(null);
+  const [councilId, setCouncilId] = useState(null);
+  const [regionCouncils, setRegionCounsils] = useState([]);
+  const [councilTypes, setCouncilTypes] = useState([]);
+
+  useEffect(() => {
+    setRegionCounsils(
+      councils.filter((council) => council.regionId == regionId)
+    );
+  }, [regionId]);
+
+  useEffect(() => {
+    setCouncilTypes(types.filter((type) => type.councilId == councilId));
+  }, [councilId]);
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Begoodsupply</title>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+      <main>
+        <h1>
+          regionId: {regionId}
+          <br />
+          councilId: {councilId}
         </h1>
+        <div>
+          <label htmlFor="region">Region</label>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+          {/* region */}
+          <select
+            id="region"
+            name="region"
+            onChange={(e) => setRegionId(e.target.value)}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            <option value="">Select a Region</option>
+            {regions.map((region) => (
+              <option key={region.id} value={region.id}>
+                {region.name}
+              </option>
+            ))}
+          </select>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+          {/* council */}
+          <label htmlFor="council">Council</label>
+
+          <select
+            id="council"
+            name="council"
+            onChange={(e) => setCouncilId(e.target.value)}
+            disabled={!regionCouncils.length}
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+            <option value="">Select a Council</option>
+            {regionCouncils.length &&
+              regionCouncils.map((council) => (
+                <option key={council.id} value={council.id}>
+                  {council.name}
+                </option>
+              ))}
+          </select>
+
+          {/* type */}
+          {councilTypes.map((type) => (
+            <p key={type.id}>
+              {type.name}: {type.value}
             </p>
-          </a>
+          ))}
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <footer></footer>
     </div>
-  )
+  );
 }
